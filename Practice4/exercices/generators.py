025bebea11d3b0eac545fa0d2c5f991fcbf1aa9c
Zhanik <janerke.erzatkyzy@gmail.com>
2026-02-27 01:50:23 +0500

    generators

--- Practice4/exercices/generators.py
+++ Practice4/exercices/generators.py
@@ -1,18 +1,44 @@
 #1
-def gen_numbers(n):
+def squares_up_to(n):
     for i in range(n + 1):
-        yield i
+        yield i * i
 
-g = gen_numbers(3)
-
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g)) 
+n = int(input())
+print(*squares_up_to(n))
 
 #2
-def powers_of_two(n):
-    for i in range(n + 1):
-        yield 2 ** i
+def evens(n):
+    for i in range(0, n + 1):
+        if i % 2 == 0:
+            yield i
 
-print(*powers_of_two(5))  # 1 2 4 8 16 32
+n = int(input())
+print(",".join(map(str, evens(n))))
+
+#3
+def div_3_4(n):
+    for i in range(n + 1):
+        if i % 12 == 0:
+            yield i
+
+n = int(input())
+print(*div_3_4(n))
+
+#4
+def squares(a, b):
+    for i in range(a, b + 1):
+        yield i * i
+
+a = int(input())
+b = int(input())
+
+for x in squares(a, b):
+    print(x)
+
+#5
+def countdown(n):
+    for i in range(n, -1, -1):
+        yield i
+
+n = int(input())
+print(*countdown(n))
