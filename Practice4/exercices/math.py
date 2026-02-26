9f36606dd6d5d69cab696668fe2016bfe54fa801
Zhanik <janerke.erzatkyzy@gmail.com>
2026-02-27 01:53:50 +0500

    math

--- Practice4/exercices/math.py
+++ Practice4/exercices/math.py
@@ -1,17 +1,30 @@
 #1
-x = min(5, 10, 25)
-y = max(5, 10, 25)
-print(x)
-print(y)
+import math
+
+deg = float(input("Input degree: "))
+rad = deg * math.pi / 180
+print(f"Output radian: {rad:.6f}")
 
 #2
-x = pow(4, 3)
-print(x)
+h = float(input("Height: "))
+b1 = float(input("Base, first value: "))
+b2 = float(input("Base, second value: "))
+
+area = (b1 + b2) / 2 * h
+print(f"Expected Output: {area}")
 
 #3
 import math
 
-x = math.ceil(1.4)
-y = math.floor(1.4)
-print(x)
-print(y)
+n = int(input("Input number of sides: "))
+s = float(input("Input the length of a side: "))
+
+area = (n * s * s) / (4 * math.tan(math.pi / n))
+print(f"The area of the polygon is: {area:g}")
+
+#4
+base = float(input("Length of base: "))
+h = float(input("Height of parallelogram: "))
+
+area = base * h
+print(f"Expected Output: {area}")
